92a1fc3913a15fe4c2741322e235e9d1f137ee33
Jasper Francisco <9131226+jfcisco@users.noreply.github.com>
2025-12-06 02:50:18 +0800

    CH1.4

--- httpfromtcp/main.go
+++ httpfromtcp/main.go
@@ -8,22 +8,45 @@ import (
 	"os"
 )
 
+var LineBreak byte = '\n'
+
 func main() {
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal("unable to open messages.txt")
 	}
 
-	// Read files 8 bytes at a time
-	buf := make([]byte, 8)
+	// to read data 8 bytes at a time
+	inputBuffer := make([]byte, 8)
+	var lineBuffer []byte
 	for {
-		numRead, err := file.Read(buf)
+		numRead, err := file.Read(inputBuffer)
 		if err != nil {
 			if !errors.Is(err, io.EOF) {
 				log.Fatalf("error while reading file: %v\n", err)
 			}
 			break
 		}
-		fmt.Printf("read: %s\n", buf[0:numRead])
+
+		// scan input buffer for any newlines
+		newlineIndex := -1
+		for i, inputChar := range inputBuffer[:numRead] {
+			if inputChar == LineBreak {
+				newlineIndex = i
+				break
+			}
+		}
+
+		if newlineIndex == -1 {
+			// no newlines so accumulate everything in line buffer
+			lineBuffer = append(lineBuffer, inputBuffer[:numRead]...)
+			continue
+		}
+
+		// newline found so flush buffer to stdout
+		lineBuffer = append(lineBuffer, inputBuffer[0:newlineIndex]...)
+		fmt.Printf("read: %s\n", lineBuffer)
+		lineBuffer = lineBuffer[:0]
+		lineBuffer = append(lineBuffer, inputBuffer[newlineIndex+1:numRead]...)
 	}
 }
