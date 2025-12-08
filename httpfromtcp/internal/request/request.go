a5ccc61d730012b105c9f787e3f11c6acba511d3
Jasper Francisco <9131226+jfcisco@users.noreply.github.com>
2025-12-08 14:11:29 +0800

    CH4.2 Request parser

--- httpfromtcp/internal/request/request.go
+++ httpfromtcp/internal/request/request.go
@@ -1,1 +1,65 @@
 package request
+
+import (
+	"errors"
+	"io"
+	"strings"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+const CRLF string = "\r\n"
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	msg, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	msgLines := strings.Split(string(msg), CRLF)
+
+	if len(msgLines) == 0 {
+		return nil, errors.New("read empty request")
+	}
+
+	// request-line should be the first line
+	// expect the format (method SP request-target SP method)
+	reqLine := msgLines[0]
+	reqLineParts := strings.Split(reqLine, " ")
+
+	if len(reqLineParts) != 3 {
+		return nil, errors.New("unexpected number of parts in request-line")
+	}
+
+	method := reqLineParts[0]
+	target := reqLineParts[1]
+	httpVer := reqLineParts[2]
+
+	// validate method is all uppercase
+	if method != strings.ToUpper(method) {
+		return nil, errors.New("method contains unsupported characters")
+	}
+
+	// validate HTTP version and strip prefix "HTTP/"
+	if httpVer != "HTTP/1.1" {
+		return nil, errors.New("unsupported HTTP version")
+	}
+	httpVer = httpVer[5:] // we only want the part after HTTP/
+
+	result := &Request{
+		RequestLine: RequestLine{
+			HttpVersion:   httpVer,
+			RequestTarget: target,
+			Method:        method,
+		},
+	}
+	return result, nil
+}
--- httpfromtcp/internal/request/request_test.go
+++ httpfromtcp/internal/request/request_test.go
@@ -1,11 +1,55 @@
 package request
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestRequestLineParse(t *testing.T) {
-	assert.Equal(t, "TheTestagen", "TheTestagen")
+func TestRequestLineParse_Good(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestRequestLineParse_GoodWithPath(t *testing.T) {
+	r, err := RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestRequestLineParse_GoodPOST(t *testing.T) {
+	sr := strings.NewReader("POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/8.17.0\r\nAccept: */*\r\nContent-Type: application/json\r\nContent-Length: 22\r\n\r\n{\"flavor\":\"dark mode\"}")
+	r, err := RequestFromReader(sr)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+}
+
+func TestRequestLineParts_InvalidCount(t *testing.T) {
+	// Test: Invalid number of parts in request line
+	_, err := RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+}
+
+func TestRequestLineParse_InvalidMethod(t *testing.T) {
+	sr := strings.NewReader("/coffee HTTP/1.1 HEAD")
+	_, err := RequestFromReader(sr)
+	require.Error(t, err)
+}
+
+func TestRequestLineParse_InvalidVersion(t *testing.T) {
+	sr := strings.NewReader("PATCH /coffee/1 TEAPOT/1.1")
+	_, err := RequestFromReader(sr)
+	require.Error(t, err)
 }
