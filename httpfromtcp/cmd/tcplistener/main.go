e73c981235d4ed2f20aa8f9c76472c5f1c7f226d
Jasper Francisco <9131226+jfcisco@users.noreply.github.com>
2025-12-09 14:33:46 +0800

    CH4.6 Parse TCP

--- httpfromtcp/cmd/tcplistener/main.go
+++ httpfromtcp/cmd/tcplistener/main.go
@@ -1,68 +1,29 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net"
-	// "strings"
+
+	"github.com/jfcisco/bootdev-projects/httpfromtcp/internal/request"
 )
 
-const INPUT_BUFFER_SIZE int = 8
+func readRequest(rc io.ReadCloser) error {
+	defer rc.Close()
 
-var LineBreak byte = '\n'
+	r, err := request.RequestFromReader(rc)
+	if err != nil {
+		return err
+	}
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	linesChan := make(chan string)
+	fmt.Println("Request line:")
+	fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+	fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+	fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 
-	// loop goroutine to read lines from `f` 8 bytes at a time
-	go func() {
-		inputBuffer := make([]byte, INPUT_BUFFER_SIZE)
-		var lineBuffer []byte
-		for {
-			numRead, err := f.Read(inputBuffer)
-			if err != nil {
-				if !errors.Is(err, io.EOF) {
-					log.Fatalf("unexpected error while reading file: %v\n", err)
-				}
-
-				// flush line buffer if needed
-				if len(lineBuffer) > 0 {
-					linesChan <- string(lineBuffer)
-				}
-
-				close(linesChan)
-				f.Close()
-				break
-			}
-
-			// scan input buffer for any newlines
-			newlineIndex := -1
-			for i, inputChar := range inputBuffer[:numRead] {
-				if inputChar == LineBreak {
-					newlineIndex = i
-					break
-				}
-			}
-
-			if newlineIndex == -1 {
-				// no newlines so accumulate everything in line buffer
-				lineBuffer = append(lineBuffer, inputBuffer[:numRead]...)
-				continue
-			}
-
-			// flush line buffer up to newline
-			lineBuffer = append(lineBuffer, inputBuffer[0:newlineIndex]...)
-			linesChan <- string(lineBuffer)
-
-			// reset line buffer, appending any remaining data in input
-			lineBuffer = lineBuffer[:0]
-			lineBuffer = append(lineBuffer, inputBuffer[newlineIndex+1:numRead]...)
-		}
-	}()
-
-	return linesChan
+	fmt.Println("connection closed")
+	return nil
 }
 
 func main() {
@@ -77,16 +38,13 @@ func main() {
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Printf("error accepting connection: %s\n", err)
-			continue
 		}
 
 		fmt.Println("connection accepted")
 
-		ch := getLinesChannel(conn)
-		for line := range ch {
-			fmt.Println(line)
+		if err := readRequest(conn); err != nil {
+			log.Printf("error from conn: %s\n", err)
+			continue
 		}
-
-		fmt.Println("connection closed")
 	}
 }
