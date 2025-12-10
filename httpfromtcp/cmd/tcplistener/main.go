80e89120e1c3432150a58a01d4a182997e07a345
Jasper Francisco <9131226+jfcisco@users.noreply.github.com>
2025-12-10 13:54:02 +0800

    Print headers in parser

--- httpfromtcp/cmd/tcplistener/main.go
+++ httpfromtcp/cmd/tcplistener/main.go
@@ -22,6 +22,11 @@ func readRequest(rc io.ReadCloser) error {
 	fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
 	fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 
+	fmt.Println("Headers:")
+	for key, value := range r.Headers {
+		fmt.Printf("- %s: %s\n", key, value)
+	}
+
 	fmt.Println("connection closed")
 	return nil
 }
